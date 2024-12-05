1738438209e879722510759bcc9dde38b88c0d8f
Sergo <rogaldh@radsh.red>
2024-12-05 15:35:47 +0000

    feat: adjust light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 const lineClamp = require('@tailwindcss/line-clamp')
 const forms = require('@tailwindcss/forms')
 
+const agridexTheme = {
+  primary: { light: '#5DC9EB', dark: '#101010'},
+  'secondary-1': { light: '#AFD803', dark: '#6CBF00' },
+  'secondary-2': { light: '#E54033', dark: '#C7251A' },
+  'fgd-1': '#3d3d3d',
+  'fgd-2': '#7a7a7a',
+  'fgd-3': '#adadad',
+  'fgd-4': '#cccccc',
+  'bkg-1': '#fcfcfc',
+  'bkg-2': '#CACADC',
+  'bkg-3': '#e0e0e0',
+  'bkg-4': '#d6d6d6',
+  orange: '#F7A531',
+  red: '#cb676f',
+  green: '#6BBF5F',
+  blue: '#183fbc',
+  discord: '#7289da',
+}
+
 module.exports = {
   darkMode: 'class',
   content: [
@@ -28,6 +47,7 @@ module.exports = {
       },
       colors: {
         discord: '#7289da',
+        'agridex-theme': agridexTheme,
         'dark-theme': {
           primary: { light: '#5DC9EB', dark: '#cecece' },
           'secondary-1': { light: '#AFD803', dark: '#6CBF00' },
