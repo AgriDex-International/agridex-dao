c6cf022311d1b4bd763627147ab3d8bbfc23dd78
Sergo <rogaldh@radsh.red>
2024-12-09 14:09:01 +0000

    feat: adjust logo position for mobile layout

--- components/AgridexLogo.tsx
+++ components/AgridexLogo.tsx
@@ -1,19 +1,26 @@
+"use client"
 import React from 'react'
 import { useTheme } from 'next-themes'
+import { useMediaQuery } from 'react-responsive';
+import cx from '@hub/lib/cx';
 
 const AgridexLogo = () => {
   const { theme = "Dark" } = useTheme()
 
+  const isMobile = useMediaQuery({ query: '(max-width: 639px)' });
+
+  const mobileClassName = React.useMemo(() => cx(isMobile ? "" : "relative", isMobile ? "" : "top-[4px]"), [isMobile]); 
+
   return (
     <>
-      <div className="p-2 text-xs">for</div>
+      <div className="pl-2 pr-2 pt-1 my-auto text-xs">for</div>
       {theme === 'Dark' ? (
         <picture>
           <source
             srcSet="/img/a-logo-white.svg"
             media="(min-width: 640px)"
           />
-          <img src="/img/a-logo-white-m.svg" className="w-8 h-8 sm:w-24" />
+          <img src="/img/a-logo-white-m.svg" className={cx("w-8 h-8 sm:w-24", mobileClassName)} />
         </picture>
       ) : (
         <picture>
@@ -21,7 +28,7 @@ const AgridexLogo = () => {
             srcSet="/img/a-logo-black.svg"
             media="(min-width: 640px)"
           />
-          <img src="/img/a-logo-black-m.svg" className="w-8 h-8 sm:w-24" />
+          <img src="/img/a-logo-black-m.svg" className={cx("w-8 h-8 sm:w-24", mobileClassName)} />
         </picture>
       )}
     </>
